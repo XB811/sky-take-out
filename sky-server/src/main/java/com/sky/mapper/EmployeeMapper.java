9157de82705a87f49ecc539c8344267b018773ba
xb <2098780588@qq.com>
2024-09-11 02:35:42 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,11 +19,21 @@ public interface EmployeeMapper {
     Employee getByUsername(String username);
 
     /**
-     * 新增员工
-     * @param employee
-     */
+    * 新增员工
+    * @param employee 
+    * @return 
+    * @Date 2024/9/11 02:04
+    */
     @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
             "values" +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
+
+    /**
+    * 员工分页查询
+    * @param employeePageQueryDTO
+    * @return Page<Employee>
+    * @Date 2024/9/11 02:04
+    */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
